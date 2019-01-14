aadf6a86e785570a677a9502f4dfc15b7aaa8ff0
KrystianUjma <kujma@virtuslab.com>
2019-01-14 13:49:35 +0100

    328-nullable-left-off-function-type-parameter-with-default-value - add class for tests

---
+++ src/main/kotlin/foo/Bar.kt
@@ -0,0 +1,10 @@
+package foo
+
+class Bar<T> {
+    val dataList = mutableListOf<T>()
+
+    open fun checkElement(
+            elem: T,
+            addFunc: ((elem: T) -> Unit)? = { dataList.add(it) }
+    ): Int = 1
+}
