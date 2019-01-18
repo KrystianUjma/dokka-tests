59b22529aa9ba76ab10dcf91d57082a82ede2db7
KrystianUjma <kujma@virtuslab.com>
2019-01-18 17:16:53 +0100

    341 (generated docs for primary constructors have duplicate copies of parameters) - add class for tests

---
+++ src/main/kotlin/bug/_341_generated_docs_for_primary_constructors_have_duplicate_copies_of_parameters/Banana.kt
@@ -0,0 +1,16 @@
+package bug._341_generated_docs_for_primary_constructors_have_duplicate_copies_of_parameters
+
+/**
+ * Just a fruit
+ *
+ * @param weight in grams
+ * @param ranking quality from 0 to 10, where 10 is best
+ * @param color default is yellow
+ */
+class Banana (
+        private val weight: Double,
+        private val ranking: Int,
+        color: String = "yellow"
+)
+
+
