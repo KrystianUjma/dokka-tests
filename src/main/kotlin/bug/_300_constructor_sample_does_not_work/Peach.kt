7b52994ba092c95bee4706a27e77fccf77458b7f
KrystianUjma <kujma@virtuslab.com>
2019-01-30 16:35:28 +0100

    300 (constructor sample does not work) - add test cases

---
+++ src/main/kotlin/bug/_300_constructor_sample_does_not_work/Peach.kt
@@ -0,0 +1,12 @@
+package bug._300_constructor_sample_does_not_work
+
+/**
+ * Description
+ *
+ * @constructor primary constructor
+ */
+class Peach() {
+    init {
+        println("peach")
+    }
+}
---
+++ src/main/kotlin/bug/_300_constructor_sample_does_not_work/Sample.kt
@@ -0,0 +1,14 @@
+package bug._300_constructor_sample_does_not_work
+
+/**
+ * description
+ *
+ * @constructor constructor
+ */
+class Sample {
+
+    init {
+        println("foo")
+    }
+
+}
