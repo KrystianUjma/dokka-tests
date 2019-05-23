77bc792bbdff9544f8ed5f2df8ea3905756f63ad
KrystianUjma <kujma@virtuslab.com>
2019-05-23 16:24:03 +0200

    #465 (Params are not properly generated for java classes.) - add tests

---
+++ src/main/java/_465_Params_are_not_properly_generated_javadoc_java/Foo.java
@@ -0,0 +1,17 @@
+package _465_Params_are_not_properly_generated_javadoc_java;
+
+/**
+ * Provide access to the service.
+ */
+public class Foo {
+
+  /**
+   * perfom request
+   *
+   * @param name     user name
+   * @param password user password
+   */
+  public void request(String name, String password) {
+  }
+
+}
---
+++ src/main/kotlin/bug/_465_Params_are_not_properly_generated_javadoc/functionParameters.kt
@@ -0,0 +1,14 @@
+package bug._465_Params_are_not_properly_generated_javadoc;
+
+/**
+ * Foo
+ */
+class Foo {
+
+    /** perfom request
+     *
+     * @param name user name
+     * @param password user password
+     */
+    fun request(name: String, password: String) {}
+}
